5e4f3dfecbd0428fe4255ef2cce5585f89cfc725
Juuliien <36441596+Juuliien@users.noreply.github.com>
2020-01-26 20:03:29 +0100

    code visual studio neural network

---
+++ TP_SudokuIA/NoyauTP/ISudokuSolver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoyauTP
+{
+    public interface ISudokuSolver
+    {
+        Sudoku Solve(Sudoku s);
+
+    }
+}
